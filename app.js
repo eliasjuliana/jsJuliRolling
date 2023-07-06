0305a493cf507b6203f77392a6ab404e11d14749
Juliana Elias <eliasjuliana@gmail.com>
2023-07-06 15:52:29 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,28 @@
+"use sctrict";
+
+console.log('Hola mundo');
+
+
+
+const usuario = "Pepe";
+//usuario = "Juan";
+
+let usuario2 = "pedro"
+
+console.log(usuario);
+
+// let respuestaUsuario;
+// respuestaUsuario = prompt("Cual es tu nombre");
+
+// console.log(respuestaUsuario);
+
+// document.write("hola")
+
+// let respuestaUsuario;
+// respuestaUsuario = prompt ("ingrese su nombre");
+
+// alert("el nombre es: " + respuestaUsuario);
+// console.log("el nombre es: " + respuestaUsuario);
+
+let hola;
+console.log(hola);
